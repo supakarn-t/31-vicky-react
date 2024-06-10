4467aac0d13e7b2b4e665502484ab11af3447378
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-06-10 15:48:35 +0700

    add time now(useEffect)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
+	const [timeNow, setTimeNow] = useState(new Date().toLocaleTimeString());
+
+	useEffect(() => {
+		setInterval(() => {
+			setTimeNow(new Date().toLocaleTimeString());
+		}, 1000);
+	}, [timeNow]);
+
 	return (
-		<nav className="flex justify-end items-center h-16 bg-sky-200 sticky top-0 z-50">
-			<ul className="flex justify-end gap-4 pr-4">
+		<nav className="flex justify-between items-center px-4 h-16 bg-sky-200 sticky top-0 z-50">
+			<p className="font-bold">Time Now: {timeNow}</p>
+			<ul className="flex justify-end gap-4">
 				<li className="h-16">
 					<Link
 						to={"/"}
