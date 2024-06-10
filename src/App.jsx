fbc00ad7927831ffcb13dd7a935548f72b202894
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-06-10 11:55:39 +0700

    route home, owner, error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import OwnerPage from "./pages/OwnerPage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 export default function App() {
 	const router = createBrowserRouter([
 		{
 			path: "/",
-			element: <NavBar />,
+			element: (
+				<>
+					<NavBar />
+					<Outlet />
+				</>
+			),
+			errorElement: <ErrorPage />,
+			children: [
+				{
+					path: "/",
+					element: <HomePage />,
+				},
+				{
+					path: "/owner",
+					element: <OwnerPage />,
+				},
+			],
 		},
 	]);
 
